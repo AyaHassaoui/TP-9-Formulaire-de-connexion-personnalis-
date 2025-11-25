82d4e04d2b561b4211800117f20653827cc5decd
AyaHassaoui <a.hassaoui9598@uca.ac.ma>
2025-11-25 16:49:15 +0100

    first commit

---
+++ src/main/java/ma/fstg/security/spring_Formulaire_demo/SpringFormulaireDemoApplication.java
@@ -0,0 +1,13 @@
+package ma.fstg.security.spring_Formulaire_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormulaireDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormulaireDemoApplication.class, args);
+	}
+
+}
